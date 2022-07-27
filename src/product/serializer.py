edd182489176b78dec82570cd10d4b65655328ec
Muhammad <samsmusa@outlook.com>
2022-07-27 12:11:38 +0600

    relatedfeild serializer

--- src/product/serializer.py
+++ src/product/serializer.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from product.models import ProductVariantPrice
+
 
 class ProductVariantPriceSerializer(serializers.Serializer):
   price = serializers.FloatField()
@@ -9,4 +11,5 @@ class ProductSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     sku = serializers.CharField()
     description = serializers.CharField()
-    
+    pvariantprice = ProductVariantPriceSerializer(many=True)
+
