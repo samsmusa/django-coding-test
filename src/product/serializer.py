51cfc5d211722b8ac699586b51c0833f5dbea686
Muhammad <samsmusa@outlook.com>
2022-07-27 12:18:28 +0600

    add serializer to productvariamt variant

--- src/product/serializer.py
+++ src/product/serializer.py
@@ -2,10 +2,23 @@ from rest_framework import serializers
 
 from product.models import ProductVariantPrice
 
+class VarintSerializer(serializers.Serializer):
+  title = serializers.CharField()
+  description = serializers.CharField()
+  active = serializers.BooleanField()
+
+
+class ProductVariantSerializer(serializers.Serializer):
+  variant_title = serializers.CharField()
+  variant = VarintSerializer()
+
 
 class ProductVariantPriceSerializer(serializers.Serializer):
   price = serializers.FloatField()
   stock = serializers.FloatField()
+  product_variant_one = ProductVariantSerializer()
+  product_variant_two = ProductVariantSerializer()
+  product_variant_three = ProductVariantSerializer()
 
 class ProductSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
